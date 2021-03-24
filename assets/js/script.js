59f3c86b53b2c032454d2534a5ed447c239abdde
samoculus <samedkaragoz11@gmail.com>
2021-03-24 09:31:22 -0400

    ya

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
-let modal = document.getElementById('work-modal');
-let openModal = document.getElementById('open-modal');
-let closeModal = document.getElementsByClassName('close-modal')[0];
+//let modal = document.getElementById('work-modal');
+//let openModal = document.getElementById('open-modal');
+//let closeModal = document.getElementsByClassName('close-modal')[0];
 
-openModal.onclick = () => {
-    modal.style.display = 'block';
-};
+//openModal.onclick = () => {
+    //modal.style.display = 'block';
+//};
 
-closeModal.onclick = () => {
-    modal.style.display = 'none';
-};
+//closeModal.onclick = () => {
+    //modal.style.display = 'none';
+//};
 
 // Close modal when user clicks anywhere outside of it.
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-};
+//window.onclick = function(event) {
+    //if (event.target == modal) {
+      //modal.style.display = "none";
+    //}
+//};
 
 setInterval(() => {
     let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
